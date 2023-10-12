2254dcfed7f9f2383ec7f70e71ca33e1320afc6d
Yusup <122696255+hunzah@users.noreply.github.com>
2023-10-12 23:31:38 +0300

    Update bubble-sort.js

--- src/sort-functions/bubble-sort.js
+++ src/sort-functions/bubble-sort.js
@@ -1,16 +1,17 @@
-
+// Функция пузырьковой сортировки
 async function bubbleSort(arr) {
   for (let i = 0; i < arr.length - 1; i++) {
     for (let j = 0; j < arr.length - i - 1; j++) {
-      // Вызываем функцию паузы она сработает при нажатии на кнопку
-      await checkPause()
+      // Вызываем функцию паузы, которая остановит сортировку при нажатии на кнопку
+      await checkPause();
+      
       if (arr[j].h > arr[j + 1].h) {
-        colorBars(arr[j], arr[j + 1]); // Подсвечиваем столбцы
+        colorBars(arr[j], arr[j + 1]); // Подсвечиваем столбцы для сравнения
         let tmp = arr[j].h;
         arr[j].h = arr[j + 1].h;
         arr[j + 1].h = tmp;
-        await sleep(); 
-        remColor(arr[j], arr[j + 1]); 
+        await sleep(); // Задержка для визуализации
+        remColor(arr[j], arr[j + 1]); // Убираем подсветку
       }
     }
   }
