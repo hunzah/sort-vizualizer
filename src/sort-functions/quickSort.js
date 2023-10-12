729a86adca7fa7f3c0e8d15f39611bae650e1a9b
Yusup <122696255+hunzah@users.noreply.github.com>
2023-10-12 23:32:19 +0300

    Update quickSort.js

--- src/sort-functions/quickSort.js
+++ src/sort-functions/quickSort.js
@@ -1,33 +1,39 @@
+// Функция быстрой сортировки
 async function quickSort(arr, lo, hi) {
-    if (lo < hi) {
-      let pi = await partition(arr, lo, hi)
-      await quickSort(arr, lo, pi)
-      await quickSort(arr, pi + 1, hi)
-    }
+  if (lo < hi) {
+    let pi = await partition(arr, lo, hi);
+    await quickSort(arr, lo, pi);
+    await quickSort(arr, pi + 1, hi);
   }
-  async function partition(arr, lo, hi) {
-    let pivot = arr[lo].h
-    let i = lo - 1
-    let j = hi + 1
-    while (1) {
-      // Вызываем функцию паузы она сработает при нажатии на кнопку
-      await checkPause()
-      do {
-        i++
-      }
-      while (arr[i].h < pivot)
-      do {
-        j = j - 1
-      }
-      while (arr[j].h > pivot)
-      if (i >= j) {
-        return j
-      }
-      colorBars(arr[i], arr[j])
-      await sleep()
-      let tmp = arr[i].h
-      arr[i].h = arr[j].h
-      arr[j].h = tmp
-      remColor(arr[i], arr[j])
+}
+
+// Функция для разделения массива
+async function partition(arr, lo, hi) {
+  let pivot = arr[lo].h;
+  let i = lo - 1;
+  let j = hi + 1;
+
+  while (1) {
+    // Вызываем функцию паузы, которая остановит сортировку при нажатии на кнопку
+    await checkPause();
+    
+    do {
+      i++;
+    } while (arr[i].h < pivot);
+
+    do {
+      j = j - 1;
+    } while (arr[j].h > pivot);
+
+    if (i >= j) {
+      return j;
     }
+
+    colorBars(arr[i], arr[j]); // Подсвечиваем столбцы для сравнения
+    await sleep(); // Задержка для визуализации
+    let tmp = arr[i].h;
+    arr[i].h = arr[j].h;
+    arr[j].h = tmp;
+    remColor(arr[i], arr[j]); // Убираем подсветку
   }
+}
