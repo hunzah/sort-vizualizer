2afe64da0c24a54b038ca788ef4f6af48188e088
Yusup <122696255+hunzah@users.noreply.github.com>
2023-10-12 23:32:04 +0300

    Update oddEvenSort.js

--- src/sort-functions/oddEvenSort.js
+++ src/sort-functions/oddEvenSort.js
@@ -1,33 +1,41 @@
-async function oddEvenSort(arr) {
-    async function swap(arr, i, j) {
-      let tmp = arr[i].h
-      arr[i].h = arr[j].h
-      arr[j].h = tmp
-    }
-    let sorted = false
-    while (!sorted) {
-      sorted = true
-      for (let i = 1; i < arr.length - 1; i += 2) {
+ // Функция 'чёт-нечет' сортировки
 
-      // Вызываем функцию паузы она сработает при нажатии на кнопку
-      await checkPause()
+ async function oddEvenSort(arr) {
+  // Функция для обмена элементов в массиве
+  async function swap(arr, i, j) {
+    let tmp = arr[i].h;
+    arr[i].h = arr[j].h;
+    arr[j].h = tmp;
+  }
 
-        if (arr[i].h > arr[i + 1].h) {
-          colorBars(arr[i], arr[i + 1])
-          await sleep()
-          swap(arr, i, i + 1)
-          sorted = false
-          remColor(arr[i], arr[i + 1])
-        }
+  let sorted = false;
+  // Запускаем цикл сортировки до тех пор, пока массив не будет отсортирован
+  while (!sorted) {
+    sorted = true;
+    
+    // Сортировка нечетных индексов
+    for (let i = 1; i < arr.length - 1; i += 2) {
+      // Вызываем функцию паузы, которая остановит сортировку при нажатии на кнопку
+      await checkPause();
+
+      if (arr[i].h > arr[i + 1].h) {
+        colorBars(arr[i], arr[i + 1]); // Подсвечиваем столбцы для сравнения
+        await sleep(); // Задержка для визуализации
+        swap(arr, i, i + 1); // Обмен элементов
+        sorted = false; // Массив мог измениться, поэтому сортировка продолжается
+        remColor(arr[i], arr[i + 1]); // Убираем подсветку
       }
-      for (let i = 0; i < arr.length - 1; i += 2) {
-        if (arr[i].h > arr[i + 1].h) {
-          colorBars(arr[i], arr[i + 1])
-          await sleep()
-          swap(arr, i, i + 1)
-          sorted = false
-          remColor(arr[i], arr[i + 1])
-        }
+    }
+
+    // Сортировка четных индексов
+    for (let i = 0; i < arr.length - 1; i += 2) {
+      if (arr[i].h > arr[i + 1].h) {
+        colorBars(arr[i], arr[i + 1]); // Подсвечиваем столбцы для сравнения
+        await sleep(); // Задержка для визуализации
+        swap(arr, i, i + 1); // Обмен элементов
+        sorted = false; // Массив мог измениться, поэтому сортировка продолжается
+        remColor(arr[i], arr[i + 1]); // Убираем подсветку
       }
     }
   }
+}
