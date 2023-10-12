774d1bf382edc7069b2af967a3fe341317476435
Yusup <122696255+hunzah@users.noreply.github.com>
2023-10-12 23:32:30 +0300

    Update shell-sort.js

--- src/sort-functions/shell-sort.js
+++ src/sort-functions/shell-sort.js
@@ -1,17 +1,22 @@
+// Функция сортировки Шелла
 async function shellSort(arr) {
-    for (var h = arr.length; h > 0; h = parseInt(h / 2)) {
-      for (var i = h; i < arr.length; i++) {
-      // Вызываем функцию паузы она сработает при нажатии на кнопку
-      await checkPause()
-      
-        var k = arr[i].h
-        for (var j = i; j >= h && k < arr[j - h].h; j -= h)
-          arr[j].h = await arr[j - h].h
-        arr[j].h = await k
-        colorBars(arr[i], arr[j])
-        await sleep()
-        remColor(arr[i], arr[j])
+  for (var h = arr.length; h > 0; h = parseInt(h / 2)) {
+    for (var i = h; i < arr.length; i++) {
+      // Вызываем функцию паузы, которая остановит сортировку при нажатии на кнопку
+      await checkPause();
+
+      var k = arr[i].h;
+
+      // Сортировка вставками с шагом h
+      for (var j = i; j >= h && k < arr[j - h].h; j -= h) {
+        arr[j].h = await arr[j - h].h;
       }
+      arr[j].h = await k;
+
+      colorBars(arr[i], arr[j]); // Подсвечиваем столбцы для сравнения
+      await sleep(); // Задержка для визуализации
+      remColor(arr[i], arr[j]); // Убираем подсветку
     }
-    return arr
   }
+  return arr;
+}
